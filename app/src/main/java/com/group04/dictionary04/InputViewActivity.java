dc00efb9b5705d80952032c9e343c1382d63036a
Lukas Trimmel <lukas.trimmel@student.tugraz.at>
2019-03-20 11:33:10 +0100

    SW-000 project setup [TL, SL]

---
+++ app/src/main/java/com/group04/dictionary04/InputViewActivity.java
@@ -0,0 +1,15 @@
+package com.group04.dictionary04;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class InputViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inputview);
+    }
+}
