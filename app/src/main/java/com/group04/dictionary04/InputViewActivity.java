4098567a2d128d1fe205305cdeeb60dddb0bc3f0
Julian Kainz <julian.kainz@student.tugraz.at>
2019-03-24 18:26:23 +0100

    LANG_001 Toast_notification_btn [KD,KJ]

--- app/src/main/java/com/group04/dictionary04/InputViewActivity.java
+++ app/src/main/java/com/group04/dictionary04/InputViewActivity.java
@@ -4,12 +4,38 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class InputViewActivity extends Activity {
+public class InputViewActivity  extends Activity  {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inputview);
+
+
+        Button button = (Button) findViewById(R.id.button_input);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast displayToast = Toast.makeText(InputViewActivity.this,"top", Toast.LENGTH_LONG);
+                displayToast.show();
+
+            }
+        });
+
+
+
     }
+
+
+
+
+
+
+
 }
