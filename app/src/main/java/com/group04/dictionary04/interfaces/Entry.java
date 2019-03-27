6a9c6dd5fb733a134686c09ed6d317f49064d906
Lukas Trimmel <lukas.trimmel@student.tugraz.at>
2019-03-27 10:12:00 +0100

    LANG-010 interface setup [TL,SL]

---
+++ app/src/main/java/com/group04/dictionary04/interfaces/Entry.java
@@ -0,0 +1,17 @@
+package com.group04.dictionary04.interfaces;
+
+public interface Entry {
+    String id1 = null;
+    String id2 = null;
+    String tag = null;
+    String rating = null;
+
+    String getId1();
+    void setId1(String id1);
+    String getId2();
+    void setId2(String id2);
+    String getTag();
+    void setTag(String tag) ;
+    String getRating();
+    void setRating(String rating);
+}
