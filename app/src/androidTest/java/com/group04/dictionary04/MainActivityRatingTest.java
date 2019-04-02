e7797eabaad48dec8f7d894ad7e0f0dca0f195c4
Matthias Wagner <matthias.wagner@student.tugraz.at>
2019-04-02 13:52:49 +0200

    LANG-004 add tests for buttons and textview [WM,WL]

---
+++ app/src/androidTest/java/com/group04/dictionary04/MainActivityRatingTest.java
@@ -0,0 +1,39 @@
+package com.group04.dictionary04;
+
+
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityRatingTest
+{
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+
+    @Test
+    public void testButtonsVisible() {
+        onView(withId(R.id.btn_ascending)).check(matches(isDisplayed()));
+        onView(withId(R.id.btn_descending)).check(matches(isDisplayed()));
+        onView(withId(R.id.btn_filter)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void textFieldsVisible(){
+        onView(withId(R.id.tv_sort)).check(matches(withText("Rate:")));
+        onView(withId(R.id.tv_sort)).check(matches(isDisplayed()));
+        onView(withId(R.id.tv_rate)).check(matches(withText("Sort:")));
+        onView(withId(R.id.tv_rate)).check(matches(isDisplayed()));
+    }
+}
