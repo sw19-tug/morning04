4bbfacef9692930a5c052f7334bc23f37963f07c
Laura Weißl <laura.weissl@student.tugraz.at>
2019-04-02 14:41:23 +0200

    LANG-004 buttons and textview implementation part one [WM,WL]

--- app/src/androidTest/java/com/group04/dictionary04/MainActivityRatingTest.java
+++ app/src/androidTest/java/com/group04/dictionary04/MainActivityRatingTest.java
@@ -1,7 +1,5 @@
 package com.group04.dictionary04;
 
-
-
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -13,13 +11,14 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityRatingTest
 {
     @Rule
-    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public ActivityTestRule<RatingViewActivity> RatingViewActivityTestRule = new ActivityTestRule<>(RatingViewActivity.class);
 
 
     @Test
