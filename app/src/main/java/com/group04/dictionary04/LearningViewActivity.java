c102543478149bb5dd39ccc1fea21ab9a8d20aaf
Lorenz Schmid <lorenz.schmid@student.tugraz.at>
2019-04-03 09:05:06 +0200

    LANG-011 On Pause/Resume/Stop [TL, SL]

--- app/src/main/java/com/group04/dictionary04/LearningViewActivity.java
+++ app/src/main/java/com/group04/dictionary04/LearningViewActivity.java
@@ -2,12 +2,51 @@ package com.group04.dictionary04;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import com.group04.dictionary04.database.DatabaseController;
+import com.group04.dictionary04.interfaces.Dictionary;
+import com.group04.dictionary04.model.default_Dictionary;
 
 public class LearningViewActivity extends Activity {
+
+
+    private default_Dictionary dict = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.learningview);
+
+        DatabaseController dbController = new DatabaseController(this.getApplicationContext());
+        dbController.saveTestDatabase();
+        Dictionary d = dbController.getCurrentDatabase();
+
+
+        Log.d("log", "Currently there are " + d.getEntries().size() + " entries in this dict");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        DatabaseController dbController = new DatabaseController(this.getApplicationContext());
+        dbController.saveTestDatabase();
+        dict = dbController.getCurrentDatabase();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        DatabaseController dbController = new DatabaseController(this.getApplicationContext());
+        dbController.saveCurrentDatabase(dict);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        DatabaseController dbController = new DatabaseController(this.getApplicationContext());
+        dbController.saveCurrentDatabase(dict);
     }
 }
--- app/src/main/java/com/group04/dictionary04/database/DatabaseController.java
+++ app/src/main/java/com/group04/dictionary04/database/DatabaseController.java
@@ -56,4 +56,6 @@ public class DatabaseController {
             return null;
         }
     }
+
+
 }
