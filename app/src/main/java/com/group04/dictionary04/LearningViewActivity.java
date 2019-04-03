da3c336449e4d226dcb6ef28348d31f750e21423
David Kerschbaumer <david.kerschbaumer@student.tugraz.at>
2019-04-03 11:19:14 +0200

    LANG_001 final version 3.0 for LANG_001  [KD,KJ]

--- app/src/main/java/com/group04/dictionary04/LearningViewActivity.java
+++ app/src/main/java/com/group04/dictionary04/LearningViewActivity.java
@@ -11,20 +11,16 @@ import com.group04.dictionary04.model.default_Dictionary;
 
 public class LearningViewActivity extends Activity {
 
-
     private default_Dictionary dict = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.learningview);
-
         DatabaseController dbController = new DatabaseController(this.getApplicationContext());
         dbController.saveTestDatabase();
         Dictionary d = dbController.getCurrentDatabase();
 
-
-        Log.d("log", "Currently there are " + d.getEntries().size() + " entries in this dict");
 
     }
 
