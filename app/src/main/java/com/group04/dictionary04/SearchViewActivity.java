41985db3f2f130b302ca1dde92f078148eaa6b18
Hugh H Sheahan <hugh.sheahan@uvm.edu>
2019-05-14 20:23:34 +0200

    LANG-003 Added some context for activity [SC, WM]

--- app/src/main/java/com/group04/dictionary04/SearchViewActivity.java
+++ app/src/main/java/com/group04/dictionary04/SearchViewActivity.java
@@ -2,8 +2,9 @@ package com.group04.dictionary04;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class SearchViewActivity extends Activity {
+public class SearchViewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/test/java/com/group04/dictionary04/Categorization/CategorizationUnitTest.java
+++ app/src/test/java/com/group04/dictionary04/Categorization/CategorizationUnitTest.java
@@ -1,4 +1,8 @@
 package com.group04.dictionary04.Categorization;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class CategorizationUnitTest {
+
 }
