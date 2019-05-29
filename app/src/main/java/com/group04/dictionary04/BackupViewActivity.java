b240c0048e769f785b8cbe514cf04ba078dfeacc
Lukas Trimmel <lukas.trimmel@student.tugraz.at>
2019-05-29 09:41:31 +0200

    LANG-009 Added sharing option [TL, WM]

--- app/src/main/java/com/group04/dictionary04/BackupViewActivity.java
+++ app/src/main/java/com/group04/dictionary04/BackupViewActivity.java
@@ -1,14 +1,21 @@
 package com.group04.dictionary04;
 
 import android.app.Activity;
+import android.content.Intent;
+
+import android.net.Uri;
 import android.os.Bundle;
 
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import com.group04.dictionary04.database.DatabaseController;
 import com.group04.dictionary04.model.default_Dictionary;
+
+import java.io.File;
 
 public class BackupViewActivity extends AppCompatActivity {
     @Override
@@ -42,6 +49,21 @@ public class BackupViewActivity extends AppCompatActivity {
 
     public void shareData(View view) {
         DatabaseController dbController = new DatabaseController(this.getApplicationContext());
+        dbController.backupDatabase();
+        File file = new File("dict04-backup");
+        //Uri contentUri = FileProvider.getUriForFile(this, "com.your.app.package", file);
+        Intent intentShareFile = new Intent(Intent.ACTION_SEND);
+
+        Log.d("as", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        intentShareFile.setType("application/text");
+        intentShareFile.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://"+"dict04-backup"));
+
+        intentShareFile.putExtra(Intent.EXTRA_SUBJECT,
+                "Sharing File...");
+        intentShareFile.putExtra(Intent.EXTRA_TEXT, "Sharing File...");
+
+        startActivity(Intent.createChooser(intentShareFile, "Share File"));
+
         //dbController.clearDatabase();
     }
 
