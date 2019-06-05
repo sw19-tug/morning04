0a4b01aee99775def036bc8493d8c557cef73c45
David Kerschbaumer <david.kerschbaumer@student.tugraz.at>
2019-06-05 10:51:03 +0200

    LANG-025 create branch and create SavedExamsTest [WM, KD]

---
+++ app/src/androidTest/java/com/group04/dictionary04/SavedExamsTest.java
@@ -0,0 +1,13 @@
+package com.group04.dictionary04;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+
+public class SavedExamsTest {
+
+    @Rule
+    public ActivityTestRule<SavedExamsViewActivity> SavedExamsViewTestRule = new ActivityTestRule<>(SavedExamsViewActivity.class);
+
+
+}
