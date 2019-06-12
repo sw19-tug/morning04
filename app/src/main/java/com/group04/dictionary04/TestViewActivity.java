a7e2d491cda4eff9f8713bc1ba9a621deee901e5
Laura Weißl <laura.weissl@student.tugraz.at>
2019-06-12 10:41:59 +0200

    LANG-027 bug fix for input without rating/tag [TL, WL]

--- app/src/main/java/com/group04/dictionary04/TestViewActivity.java
+++ app/src/main/java/com/group04/dictionary04/TestViewActivity.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.app.AppComponentFactory;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -13,9 +14,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.group04.dictionary04.database.DatabaseController;
+import com.group04.dictionary04.interfaces.Exam;
 import com.group04.dictionary04.model.default_Dictionary;
 import com.group04.dictionary04.model.default_Entry;
 import com.group04.dictionary04.model.default_Exam;
@@ -50,7 +53,7 @@ public class TestViewActivity extends AppCompatActivity implements View.OnClickL
        //--------------------------------------------------------
         Intent intent = getIntent();
         String data = intent.getStringExtra("data");
-
+        boolean valid = true;
         if(data != null){
 
             Gson gson = new Gson();
@@ -58,19 +61,31 @@ public class TestViewActivity extends AppCompatActivity implements View.OnClickL
             Log.d("asdf", data);
         }
         else{
-
-            exam = dict.generateExam(null);
+            if(dict.getEntries().size() == 0)
+            {
+                Toast.makeText(getApplicationContext(), "no data available", Toast.LENGTH_SHORT).show();
+                valid = false;
+                Intent myIntent = new Intent(TestViewActivity.this, MainActivity.class);
+                startActivity(myIntent);
+                finishAffinity();
+            } else
+                exam = dict.generateExam(null);
         }
         //--------------------------------------------------------
+        if(valid) {
+            lang1 = findViewById(R.id.textView3);
+            lang2 = findViewById(R.id.textView5);
+            givenVocab = findViewById(R.id.textView4);
+            input = findViewById(R.id.editText2);
 
-        lang1 = findViewById(R.id.textView3);
-        lang2 = findViewById(R.id.textView5);
-        givenVocab = findViewById(R.id.textView4);
-        input = findViewById(R.id.editText2);
+            if(exam.getVocsToTest().size() > 0) {
+                lang1.setText(exam.getVocsToTest().get(index).getId1().getLangString());
+                lang2.setText(exam.getVocsToTest().get(index).getId2().getLangString());
+                givenVocab.setText(exam.getVocsToTest().get(0).getId1().getValue());
+            }
+        }
 
-        lang1.setText(exam.getVocsToTest().get(index).getId1().getLangString());
-        lang2.setText(exam.getVocsToTest().get(index).getId2().getLangString());
-        givenVocab.setText(exam.getVocsToTest().get(0).getId1().getValue());
+
     }
 
     @Override
